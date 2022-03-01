7a19cdc358804d7cddfe71637fac4a23d8bc1c68
Nikhil Bhathi <92315880+dsNikhilds@users.noreply.github.com>
2022-03-01 19:22:50 -0400

    Add files via upload

---
+++ Day 1/main.py
@@ -0,0 +1,19 @@
+# print function in python
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# or
+
+print("Day 1 - Python Print function \nThe function is declared like this: \nprint('what to print')")
+
+# String Concatenation
+
+print("Day 1 - Python Print function" + "The function is declared like this:" + "print('what to print')")
+
+# Input function
+# input() will get user input in console
+
+a = input("What is your name ")
+print("Hello " + a + "!")
